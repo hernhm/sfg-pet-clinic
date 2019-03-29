4cc273972a82f31c78e5bb6f6618a998d31c608e
Michael H <47344693+hernhm@users.noreply.github.com>
2019-03-29 19:20:13 -0400

    Add files via upload

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -8,7 +8,6 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -76,17 +75,6 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner findByLastName(String lastName) {
-        return this.findAll()
-                .stream()
-                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
-                .findFirst()
-                .orElse(null);
-    }
-
-    @Override
-    public List<Owner> findAllByLastNameLike(String lastName) {
-
-        //todo - impl
         return null;
     }
 }
